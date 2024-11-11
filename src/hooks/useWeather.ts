712effc537ba348ddfa1c78de6b087a9d9d5bc20
Jose Guerrero <edgardo30g@gmail.com>
2024-11-11 15:49:59 -0600

    Agregando un Spinner de carga

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -27,10 +27,12 @@ export default function useWeather() {
         }
     })
 
+    const [loading, setLoading] = useState(false)
+
     const fetchWeather = async (search: SearchType) => {
 
         const appId = import.meta.env.VITE_API_KEY
-
+        setLoading(true)
         try{
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
@@ -49,6 +51,8 @@ export default function useWeather() {
             }
         }catch (error){
             console.log(error)
+        }finally{
+            setLoading(false)
         }
 
         console.log('Consultando...')
@@ -58,6 +62,7 @@ export default function useWeather() {
 
     return{
         weather,
+        loading,
         fetchWeather,
         hasWeatherData
     }
